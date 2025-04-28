d468cbff8301ea1cd0fd3323fd6c80cc594e8ee5
lucky kurniawan <luckykrnwn17@gmail.com>
2025-04-28 13:09:21 +0700

    feat:add config shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+import { Label } from "@/components/ui/label";
 
 export default function Home() {
   return (
@@ -49,6 +53,19 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <div>
+            <div className="w-fit">
+              <Button variant={"outline"}>Click me</Button>
+            </div>
+            <div className="w-[300px]">
+              <Button>Click me</Button>
+            </div>
+            
+          </div>
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="email">Email</Label>
+            <Input type="email" id="email" placeholder="Email" />
+          </div>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
@@ -97,7 +114,6 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-
       </footer>
     </div>
   );
