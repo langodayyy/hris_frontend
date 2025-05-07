04a9dedf150a335016a5d7204e611ad7e01a54ec
lucky kurniawan <luckykrnwn17@gmail.com>
2025-05-07 10:03:21 +0700

    feat: make component approval

--- src/components/ui/approval.tsx
+++ src/components/ui/approval.tsx
@@ -1,36 +1,36 @@
 interface ApprovalStatusBadgeProps {
-    status: "Approved" | "Pending" | "Rejected";
-  }
-  
-  export function ApprovalStatusBadge({ status }: ApprovalStatusBadgeProps) {
-    const statusStyles = {
-      Approved: {
-        bgColor: "bg-green-100",
-        textColor: "text-sm text-success-700",
-        iconColor: "bg-green-600",
-      },
-      Pending: {
-        bgColor: "bg-warning-50",
-        textColor: "text-sm text-warning-500",
-        iconColor: "bg-warning-500",
-      },
-      Rejected: {
-        bgColor: "bg-red-100",
-        textColor: "text-sm text-danger-700",
-        iconColor: "bg-red-600",
-      },
-    };
-  
-    const { bgColor, textColor, iconColor } = statusStyles[status];
-  
-    return (
-      <div className="w-full flex items-center justify-center">
-        <div
-          className={`flex items-center gap-2 px-3 py-1 w-fit rounded-2xl ${bgColor}`}
-        >
-          <span className={`w-2 h-2 rounded-full ${iconColor}`}></span>
-          <span className={`${textColor} font-medium`}>{status}</span>
-        </div>
+  status: "Approved" | "Pending" | "Rejected";
+}
+
+export function ApprovalStatusBadge({ status }: ApprovalStatusBadgeProps) {
+  const statusStyles = {
+    Approved: {
+      bgColor: "bg-green-100",
+      textColor: "text-sm text-success-700",
+      iconColor: "bg-green-600",
+    },
+    Pending: {
+      bgColor: "bg-warning-50",
+      textColor: "text-sm text-warning-500",
+      iconColor: "bg-warning-500",
+    },
+    Rejected: {
+      bgColor: "bg-red-100",
+      textColor: "text-sm text-danger-700",
+      iconColor: "bg-red-600",
+    },
+  };
+
+  const { bgColor, textColor, iconColor } = statusStyles[status];
+
+  return (
+    <div className="w-full flex items-center justify-center">
+      <div
+        className={`flex items-center gap-2 px-3 py-1 w-fit rounded-2xl ${bgColor}`}
+      >
+        <span className={`w-2 h-2 rounded-full ${iconColor}`}></span>
+        <span className={`${textColor} font-medium`}>{status}</span>
       </div>
-    );
-  }
+    </div>
+  );
+}
