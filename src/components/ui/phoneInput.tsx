3d151fefdffbfd96bc6d42b01386314c357c8fbc
mumtazharis <ahmadmumtazhrs@gmail.com>
2025-06-01 23:21:31 +0700

    feat: add input hidden on phoneinput component

--- src/components/ui/phoneInput.tsx
+++ src/components/ui/phoneInput.tsx
@@ -21,6 +21,8 @@ export default function FormPhoneInput({
         defaultCountry="ID"
         className="file:text-neutral-900 border-neutral-300 placeholder:text-neutral-300 selection:bg-primary selection:text-primary-foreground dark:bg-input/30 flex w-full min-w-0 rounded-md border bg-transparent px-4 py-3 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
       />
+      {/* Hidden input for form submission */}
+      <input type="hidden" name="phone" value={value ?? ""} />
     </div>
   );
 }
