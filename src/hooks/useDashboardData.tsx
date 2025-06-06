d724319138e529c5470bd00a8f4829f6f8ef24cd
mkemaln <kemalmuhammad2509@gmail.com>
2025-06-06 21:30:47 +0700

    feat: add logic if the token didnt valid on dashboard redirect to sign in

--- src/hooks/useDashboardData.tsx
+++ src/hooks/useDashboardData.tsx
@@ -1,21 +1,49 @@
 import { useEffect, useState } from "react";
 import { DashboardResponse } from "@/types/dashboard";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
 
 export function useDashboardData() {
-  const [dashboardData, setDashboardData] = useState<DashboardResponse|null>(null);
+  const [dashboardData, setDashboardData] = useState<DashboardResponse | null>(
+    null
+  );
   const [loading, setLoading] = useState(true);
+  const router = useRouter();
 
   useEffect(() => {
     async function fetchDashboard() {
-        try {
-            const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/dashboardnologin`);
-            const json = await res.json();
-            console.log(json);
-            setDashboardData(json);
-            setLoading(false);
-        } catch (error) {
-            console.error("Error initializing token checker:", error);
+      try {
+        const userCookie = Cookies.get("token");
+        if (userCookie) {
+          const res = await fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/dashboard`,
+            {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${userCookie}`,
+              },
+            }
+          );
+
+          if (res.status == 401) {
+            router.push("/sign-in");
+            return;
+          }
+
+          const json = await res.json();
+          console.log(json);
+          setDashboardData(json);
+        } else {
+          router.push("/sign-in");
+          // tambahin toast error
+          return;
         }
+      } catch (error) {
+        console.error("Error initializing token checker:", error);
+      } finally {
+        setLoading(false);
+      }
     }
 
     fetchDashboard();
